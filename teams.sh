d680843d32f5315b9877fb2b1f7ee76ee28a1662
Roman Anasal <roman.anasal@bdsu.de>
2020-12-27 20:38:08 +0100

    execute calls to teams in already running container
    check if a teams container is already running and exec a call to teams
    in it -> this allows to open deeplinks with the running container

--- teams.sh
+++ teams.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# check if teams container is running and use existing container if so
+if docker exec teams true > /dev/null 2>&1; then
+	docker exec teams teams "$@"
+	exit $?
+fi
+
 # ensure xdg socket is setup correctly
 rm -f ~/x11docker/teams/.xdg.sock
 mkfifo ~/x11docker/teams/.xdg.sock
