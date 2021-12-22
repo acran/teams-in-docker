c55dbe520b9e6eb476cdbabbf51a2a8060b7bf9e
Roman Anasal <roman.anasal@bdsu.de>
2021-12-22 17:38:49 +0100

    reduce capabilities even more and disable sandboxes
    starting with version 1.4.00.13653 accessing the camera only works with
    --disable-namespace-sandbox and --disable-setuid-sandbox
    These flags are set by the /usr/bin/teams wrapper script anyways.
    Additionally this now allows us to drop even more capabilities for the
    container.

--- teams.sh
+++ teams.sh
@@ -19,7 +19,7 @@ fi
 # read loop for opening urls with xdg
 while [ -p "$CONTAINER_HOME/.xdg.sock" ]; do read url < "$CONTAINER_HOME/.xdg.sock"; xdg-open "$url"; done &
 
-x11docker --pulseaudio --webcam --hostdisplay --clipboard --gpu --env LANG --home --name teams -- --cap-add=CAP_SYS_ADMIN --cap-add=CAP_SYS_CHROOT teams /usr/share/teams/teams "$@"
+x11docker --pulseaudio --webcam --hostdisplay --clipboard --gpu --env LANG --home --name teams -- teams /usr/share/teams/teams --disable-namespace-sandbox --disable-setuid-sandbox "$@"
 
 # cleanup xdg socket
 kill %1 # kill xdg-open loop
